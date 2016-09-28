512f8336558adb9557a8fe8e2db5bd3511410dbf
Murthy <212489116@logon.ds.ge.com>
2016-09-28 13:09:30 +0530

    FloorMap changes

--- PSN Buddy/www/app/routes.js
+++ PSN Buddy/www/app/routes.js
@@ -1,24 +1,40 @@
 define([
   'app',
   // Load Controllers here
-  'controllers/dashboard'
-], function (app) {
+  'controllers/dashboard',
+  'controllers/floorMap'    
+], 
+       
+
+function (app) {
   'use strict';
   // definition of routes
-  app.config([
-    '$stateProvider',
-    '$urlRouterProvider',
+  app.config(['$stateProvider','$urlRouterProvider',
+    
+
     function ($stateProvider, $urlRouterProvider) {
       // url routes/states
       $urlRouterProvider.otherwise('dashboard');
 
       $stateProvider
         // app states
-        .state('dashboard', {
+        
+    .state('dashboard', {
           url: '/dashboard',
           templateUrl: 'app/templates/dashboard.html',
           controller: 'DashboardCtrl'
-        });
+        })
+      
+      
+    .state('floormap', {
+            url: '/floormap',
+            templateUrl: 'app/templates/floorMap.html',
+            controller: 'floorMapCtrl',
+            cache: false
+      })
+      
+      
+      
     }
   ]);
 });
