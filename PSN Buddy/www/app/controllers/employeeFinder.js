6eb0e3218e1e4671cce8fd41d0fe013288f94ff9
Murthy <212489116@logon.ds.ge.com>
2016-10-03 23:10:20 +0530

    Employee Finder - UI

--- PSN Buddy/www/app/controllers/employeeFinder.js
+++ PSN Buddy/www/app/controllers/employeeFinder.js
@@ -3,21 +3,42 @@ define(['app', 'services/employeeFinderService'],
 function (app) {
   'use strict';
     
-app.controller('empFinderCtrl', ['$scope', 'empFinderService', function($scope, empFinderService){    
+app.controller('empFinderCtrl', ['$scope','empFinderService','$ionicPopup', function($scope, empFinderService,$ionicPopup){    
+    
      $scope.data={};
+     $scope.showResult = false;
+     $scope.empList = [];
     
      $scope.doSearch = function(){
         
         var sso_selection = $scope.data.sso;
         var fn_selection = $scope.data.firstName;
         var ln_selection = $scope.data.lastName;
-        
-       // empFinderFactory.get(sso_selection,fn_selection,ln_selection);
+         
+         $scope.empList = empFinderService.findEmployeeLocationDetails();
+         $scope.showResult = true;
     }
-    
-    
-}])
+     
 
+     
+     $scope.showAlert = function(index) {
+         
+        $scope.selectedItem = angular.copy($scope.empList[index]);
+      
+        var alertPopup = $ionicPopup.alert({
+        title: $scope.selectedItem.lastName+","+$scope.selectedItem.firstName,
+        template: '<table>' +
+            '<tr> <td> LastName : </td> <td>'+ $scope.selectedItem.lastName +'</td> </tr>' +
+            '<tr> <td> FirstName : </td> <td>'+ $scope.selectedItem.firstName+ '</td> </tr>'+
+            '<tr> <td> SSO : </td> <td>'+ $scope.selectedItem.sso +'</td> </tr>'+ 
+            '<tr> <td> Floor : </td> <td>'+ $scope.selectedItem.floor+ '</td> </tr>'+ 
+            '<tr> <td> Wing : </td> <td>'+ $scope.selectedItem.wing +'</td> </tr>'+ 
+            '<tr> <td> Cubicle# : </td> <td>'+ $scope.selectedItem.cubicle+ '</td> </tr>'+
+            '</table>',
+       });
+    };
+}]);
+    
 
 });
                
--- PSN Buddy/www/app/services/employeeFinderService.js
+++ PSN Buddy/www/app/services/employeeFinderService.js
@@ -4,9 +4,28 @@ define([
   'use strict';    
   
     app.service('empFinderService',[
-        function () {
-             alert("in employee finder service");
+       function ($scope) {
+           
+           this.findEmployeeLocationDetails = function($scope){
+             
+            //testing start
+             var employeeList = [];
+             var i = 0;
+             var firstName = "Shruthi", lastName = "", sso = "";
+             var floor = "", wing = "", cubicleDetails = "";
+             
+             for(i = 0; i <8; i++){
+                 
+                 lastName = "Murthy_"+i;
+                 wing = "L_"+i;
+                 employeeList[i] = {"firstName" : firstName, "lastName" : lastName, "sso" : i,
+                                     "floor" : i, "wing" : wing , "cubicle" : i };
+             }
+             //testing end
+           
+             return employeeList;
+            
              //return $resource('http://localhost:5000/employeeFinder/find');
-        }])
+           }}])
     
 });
