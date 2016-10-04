7ded6ac46912164c17e540ff5d87f558b46c5b82
Murthy <212489116@logon.ds.ge.com>
2016-10-04 17:12:54 +0530

    Employee Finder

--- PSN Buddy/www/app/controllers/employeeFinder.js
+++ PSN Buddy/www/app/controllers/employeeFinder.js
@@ -3,21 +3,32 @@ define(['app', 'services/employeeFinderService'],
 function (app) {
   'use strict';
     
-app.controller('empFinderCtrl', ['$scope','empFinderService','$ionicPopup', function($scope, empFinderService,$ionicPopup){    
+app.controller('empFinderCtrl', ['$scope','empFinderService','$ionicPopup','$http', function($scope, empFinderService,$ionicPopup,$http){    
     
      $scope.data={};
      $scope.showResult = false;
      $scope.empList = [];
     
+    
+     $scope.resetPrev = function(){
+         $scope.showResult = false;
+         $scope.empList = [];
+         $scope.data={};
+     }
+    
+     
      $scope.doSearch = function(){
         
         var sso_selection = $scope.data.sso;
         var fn_selection = $scope.data.firstName;
         var ln_selection = $scope.data.lastName;
+             
+        empFinderService.findEmployeeLocationDetails($http,sso_selection,fn_selection,ln_selection).success(function(result) {
+               $scope.empList = result;
+            });
          
-         $scope.empList = empFinderService.findEmployeeLocationDetails();
          $scope.showResult = true;
-    }
+     }
      
 
      
@@ -26,14 +37,14 @@ app.controller('empFinderCtrl', ['$scope','empFinderService','$ionicPopup', func
         $scope.selectedItem = angular.copy($scope.empList[index]);
       
         var alertPopup = $ionicPopup.alert({
-        title: $scope.selectedItem.lastName+","+$scope.selectedItem.firstName,
+        title: $scope.selectedItem.last_name+","+$scope.selectedItem.first_name,
         template: '<table>' +
-            '<tr> <td> LastName : </td> <td>'+ $scope.selectedItem.lastName +'</td> </tr>' +
-            '<tr> <td> FirstName : </td> <td>'+ $scope.selectedItem.firstName+ '</td> </tr>'+
+            '<tr> <td> LastName : </td> <td>'+ $scope.selectedItem.last_name +'</td> </tr>' +
+            '<tr> <td> FirstName : </td> <td>'+ $scope.selectedItem.first_name+ '</td> </tr>'+
             '<tr> <td> SSO : </td> <td>'+ $scope.selectedItem.sso +'</td> </tr>'+ 
             '<tr> <td> Floor : </td> <td>'+ $scope.selectedItem.floor+ '</td> </tr>'+ 
-            '<tr> <td> Wing : </td> <td>'+ $scope.selectedItem.wing +'</td> </tr>'+ 
-            '<tr> <td> Cubicle# : </td> <td>'+ $scope.selectedItem.cubicle+ '</td> </tr>'+
+            '<tr> <td> Wing : </td> <td>'+ $scope.selectedItem.wings +'</td> </tr>'+ 
+            '<tr> <td> Cubicle# : </td> <td>'+ $scope.selectedItem.cubicle_number+ '</td> </tr>'+
             '</table>',
        });
     };
--- PSN Buddy/www/app/services/employeeFinderService.js
+++ PSN Buddy/www/app/services/employeeFinderService.js
@@ -4,28 +4,10 @@ define([
   'use strict';    
   
     app.service('empFinderService',[
-       function ($scope) {
+       function () {
            
-           this.findEmployeeLocationDetails = function($scope){
-             
-            //testing start
-             var employeeList = [];
-             var i = 0;
-             var firstName = "Shruthi", lastName = "", sso = "";
-             var floor = "", wing = "", cubicleDetails = "";
-             
-             for(i = 0; i <8; i++){
-                 
-                 lastName = "Murthy_"+i;
-                 wing = "L_"+i;
-                 employeeList[i] = {"firstName" : firstName, "lastName" : lastName, "sso" : i,
-                                     "floor" : i, "wing" : wing , "cubicle" : i };
-             }
-             //testing end
-           
-             return employeeList;
-            
-             //return $resource('http://localhost:5000/employeeFinder/find');
+           this.findEmployeeLocationDetails = function($http, sso_selection,fn_selection,ln_selection){
+               return $http.post("http://localhost:5000/employeeFinder/find", {sso:sso_selection, fname: fn_selection,lname: ln_selection});     
            }}])
     
 });
